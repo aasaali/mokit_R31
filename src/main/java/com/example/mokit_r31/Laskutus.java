c4a1b7c56f8583cb2505fbe4d802ffd59f4ddef2
Taija <taijamie@uef.fi>
2023-04-21 11:14:58 +0300

    Muutettu luokan nimi.

--- src/main/java/com/example/mokit_r31/Laskutus.java
+++ src/main/java/com/example/mokit_r31/Laskutus.java
@@ -1,6 +1,6 @@
 package com.example.mokit_r31;
 
-public class testi {
+public class Laskutus {
 
     public static void main(String[] args) {
         System.out.println("Minä olen muokannut tätä tekstiä! :)");
