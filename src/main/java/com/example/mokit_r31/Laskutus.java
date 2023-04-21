15794d63a9732ce954054dd251a3c93a9bb65e8f
Taija <taijamie@uef.fi>
2023-04-21 22:04:35 +0300

    Luokat asiakkaille ja asiakashallinnan metodeille

--- src/main/java/com/example/mokit_r31/Laskutus.java
+++ src/main/java/com/example/mokit_r31/Laskutus.java
@@ -2,6 +2,11 @@ package com.example.mokit_r31;
 
 public class Laskutus {
 
+    // Laskun tietoja:
+    // asiakkaan tiedot: nimi, osoite, puhelinnumero
+    // Varauksen tiedot: pvm-pvm, kohde, palvelut, hintaerittely
+    // laskutus: summa, pvm, laskun numero, eräpäivä pvm + 14 vrk
+
     public static void main(String[] args) {
         System.out.println("Tähän tulee laskunhallintajärjestelmän luokka.");
     }
