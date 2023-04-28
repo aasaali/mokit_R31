26aa94c73d5ab2588d0ffc37fa79bd39aa1ceecb
aatusaali <asaali@uef.fi>
2023-04-28 08:38:54 +0300

    fxml - varauksille ja alustava controller

---
+++ src/main/java/com/example/mokit_r31/VarausController.java
@@ -0,0 +1,8 @@
+package com.example.mokit_r31;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class VarausController {
+    FXML
+    public void tallennaVarausButton() {
+    }
+}
