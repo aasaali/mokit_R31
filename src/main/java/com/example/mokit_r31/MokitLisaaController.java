590f150ca9d0dbc34899aa85712d908b164ccbe5
juuli <ahonenj@uef.fi>
2023-05-05 10:20:25 +0300

    Mokkien ja aluiden ikkunat

---
+++ src/main/java/com/example/mokit_r31/MokitLisaaController.java
@@ -0,0 +1,36 @@
+package com.example.mokit_r31;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class MokitLisaaController {
+    @FXML
+    private Button BtPaluuMain;
+    @FXML
+    private Button BtTallenna;
+    @FXML
+    private Button BtPoista;
+
+    @FXML
+    private TextField TfMokinNimi;
+    @FXML
+    private TextField TfHlo;
+    @FXML
+    private TextField TfMokinId;
+    @FXML
+    private TextField TfHinta;
+    @FXML
+    private TextField TfAlueId;
+    @FXML
+    private TextField TfKatuosoite;
+    @FXML
+    private TextField TfPostiNro;
+    @FXML
+    private TextField TfKuvaus;
+    @FXML
+    private TextField TfVarustelu;
+
+
+}
