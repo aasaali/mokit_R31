d5bf8d8bfa293fbdb88b37eeadff76413131b567
aatusaali <asaali@outlook.com>
2023-05-08 13:56:24 +0300

    Palveluiden raportointi tuölkalu valmis

--- src/main/java/com/example/mokit_r31/PalvelutRaporttiController.java
+++ src/main/java/com/example/mokit_r31/PalvelutRaporttiController.java
@@ -1,10 +1,21 @@
 package com.example.mokit_r31;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class PalvelutRaporttiController {
@@ -24,6 +35,10 @@ public class PalvelutRaporttiController {
     @FXML
     private Button haeButton;
 
+    @FXML
+    private Button tallennaPDFButton;
+
+
     public void initialize() {
         // Ladataan alueet comboboxiin
         PalvelutRaportti.lataaAlueetComboBoxiin(aluecbox);
@@ -38,6 +53,55 @@ public class PalvelutRaporttiController {
             // Haetaan raportti palveluista
             PalvelutRaportti.haePalveluRaportti(alue, alkupaiva, loppupaiva, palvelutLw);
         });
+
+        tallennaPDFButton.setOnAction(e -> {
+            String alue = aluecbox.getValue();
+            String alkupaiva = alkupaivaDP.getValue().toString();
+            String loppupaiva = loppupaivaDP.getValue().toString();
+
+            // Tallennetaan raportti PDF-tiedostoksi
+            tallennaPDF(alue, alkupaiva, loppupaiva, palvelutLw);
+        });
     }
 
+
+    public static void tallennaPDF(String alue, String alkupaiva, String loppupaiva, ListView<String> palvelutLw) {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String raportinNimi = "Palveluraportti_" + alue + "_" + dateFormat.format(new Date()) + ".pdf";
+
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream(raportinNimi));
+            document.open();
+
+            // Otsikko
+            Paragraph otsikko = new Paragraph("Palveluraportti", new Font(Font.FontFamily.HELVETICA, 18));
+            otsikko.setAlignment(Paragraph.ALIGN_CENTER);
+            document.add(otsikko);
+            document.add(new Paragraph("\n"));
+
+            // Alue
+            Paragraph alueP = new Paragraph("Alue: " + alue);
+            document.add(alueP);
+            document.add(new Paragraph("\n"));
+
+            // Ajanjakso
+            Paragraph ajanjaksoP = new Paragraph("Ajanjakso: " + alkupaiva + " - " + loppupaiva);
+            document.add(ajanjaksoP);
+            document.add(new Paragraph("\n"));
+
+            // Palvelut
+            document.add(new Paragraph("Palvelut:"));
+            document.add(new Paragraph("\n"));
+
+            for (String palvelu : palvelutLw.getItems()) {
+                document.add(new Paragraph(palvelu));
+            }
+
+            document.close();
+            System.out.println("Raportti tallennettu tiedostoon: " + raportinNimi);
+        } catch (DocumentException | IOException e) {
+        }
+
+    }
 }
